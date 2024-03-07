e3c4ad82fb46940628666142898d45375cc4b67c
Animesh239 <animeshbarik239@gmail.com>
2024-03-08 02:24:31 +0530

    added register with google

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import logo from "./logo.svg";
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
 import { NavBar } from "./components/NavBar";
 import { Banner } from "./components/Banner";
 import { Skills } from "./components/Skills";
 import { Projects } from "./components/Projects";
 import { Contact } from "./components/Contact";
 import { Footer } from "./components/Footer";
+import UserMenu from "./components/UserMenu";
+import Login from "./components/Login";
+import { AuthProvider } from "./context/authProvider";
 
 function App() {
   return (
-    <div className="App">
-      <NavBar />
-      <Banner />
-      <Skills />
-      <Projects />
-      {/* <Contact /> */}
-      <Footer />
-    </div>
+    <AuthProvider>
+      <div className="App">
+        <NavBar />
+        <Banner />
+        <Skills />
+        <Projects />
+        {/* <Contact /> */}
+        <Login />
+        <UserMenu />
+        <Footer />
+      </div>
+    </AuthProvider>
   );
 }
 
