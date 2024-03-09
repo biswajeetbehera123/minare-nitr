65b4c4df3a031718f43b3e99233fd3562c169c48
Nidhi0604 <np060402@gmail.com>
2024-03-09 11:56:42 +0530

    events box design changed

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -17,7 +17,7 @@ import image4 from "../assets/img/image 4.png";
 import image11 from "../assets/img/image 11.png";
 import image10 from "../assets/img/image 11.png";
 import R from "../assets/img/R.png";
-import im3 from "../assets/img/im3.png";
+
 import JSW from "../assets/img/JSW.png";
 
 
@@ -80,10 +80,7 @@ export const Skills = () => {
                                 <img src={image4} alt="Image" />
                                 <h5>IMFA</h5>
                             </div>
-                            <div className="item">
-                                <img src={im3} alt="Image" />
-                                <h5>MOIL</h5>
-                            </div>
+                            
                             <div className="item">
                                 <img src={image11} alt="Image" />
                                 <h5>ALTRADE</h5>
