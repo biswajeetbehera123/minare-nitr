49422f5427b647f846863991a4eeae8faf134c79
AbinashPadhan05 <123665054+AbinashPadhan05@users.noreply.github.com>
2024-03-11 13:45:25 +0530

    photos added

--- src/pages/TeamsPage.js
+++ src/pages/TeamsPage.js
@@ -14,52 +14,77 @@ export default function Teams(){
     const teamData = [
         {
             id : '1',
-            name: 'K Uma Maheshwar Rao',
-            position: 'Director',
+            name: 'Prof. K. Umamaheshwar Rao',
+            position: 'Director, Patron (Ex-officio)',
             imgurl: 'https://www.nitrkl.ac.in/assets/images/director.jpg'
         },
         {
             id : '2',
-            name: 'K Uma Maheshwar Rao',
-            position: 'Director',
-            imgurl: 'https://www.nitrkl.ac.in/assets/images/director.jpg'
+            name: 'Prof. A. K. Gorai',
+            position: 'Head of the Department, President (Ex-officio) ',
+            imgurl: 'https://res.cloudinary.com/dei7rxeug/image/upload/v1710144647/minare/soaqr9egx9v53kwpl4sw.jpg'
         },
         {
             id : '3',
-            name: 'K Uma Maheshwar Rao',
-            position: 'Director',
-            imgurl: 'https://www.nitrkl.ac.in/assets/images/director.jpg'
+            name: 'Prof. H. B. Sahu',
+            position: 'Adviser',
+            imgurl: 'https://res.cloudinary.com/dei7rxeug/image/upload/v1710144546/minare/a1uq1txt3hpvddzubrng.jpg'
         },
         {
             id : '4',
-            name: 'K Uma Maheshwar Rao',
-            position: 'Director',
-            imgurl: 'https://www.nitrkl.ac.in/assets/images/director.jpg'
+            name: 'Prof. C. K. Ala',
+            position: 'Treasurer',
+            imgurl: 'https://res.cloudinary.com/dei7rxeug/image/upload/v1710017177/minare/bqimec7k5wbwi9dtjdxd.png'
         },
         {
             id : '5',
-            name: 'K Uma Maheshwar Rao',
-            position: 'Director',
-            imgurl: 'https://www.nitrkl.ac.in/assets/images/director.jpg'
+            name: 'Aryaman Biswal',
+            position: 'Vice-President ',
+            imgurl: 'https://res.cloudinary.com/dei7rxeug/image/upload/v1710017062/minare/myutn9fpt0hvyunwzmdb.jpg'
         },
         {
             id : '6',
-            name: 'K Uma Maheshwar Rao',
-            position: 'Director',
-            imgurl: 'https://www.nitrkl.ac.in/assets/images/director.jpg'
+            name: 'Satyabrata Biswal',
+            position: 'Secretary',
+            imgurl: 'https://res.cloudinary.com/dei7rxeug/image/upload/v1710017063/minare/kbv2ua8bpinpj2mbeocr.jpg'
         },
         {
             id : '7',
-            name: 'K Uma Maheshwar Rao',
-            position: 'Director',
-            imgurl: 'https://www.nitrkl.ac.in/assets/images/director.jpg'
+            name: 'Kanha Kalingo',
+            position: 'Joint Secretary',
+            imgurl: 'https://res.cloudinary.com/dei7rxeug/image/upload/v1710017064/minare/lvawudfyup6ktxajrmz0.jpg'
         },
         {
+           
             id : '8',
-            name: 'K Uma Maheshwar Rao',
-            position: 'Director',
-            imgurl: 'https://www.nitrkl.ac.in/assets/images/director.jpg'
+            name: 'Tavishi Agarwal',
+            position: 'Joint Secretary',
+            imgurl: 'https://res.cloudinary.com/dei7rxeug/image/upload/v1710017061/minare/dp6l5wzejgf4nvbtyfvc.jpg'
         },
+        {
+           
+            id : '9',
+            name: 'Baishakhi Biswal',
+            position: 'Cultural Secretary',
+            imgurl: 'https://res.cloudinary.com/dei7rxeug/image/upload/v1710017063/minare/qftfid7wozcmrrrurk5b.jpg'
+        },
+        {
+           
+            id : '10',
+            name: 'Piyush Kumar',
+            position: 'Literary Secretary',
+            imgurl: 'https://res.cloudinary.com/dei7rxeug/image/upload/v1710017061/minare/umeof1jka9eijnsl16x0.jpg'
+        },
+        {
+           
+            id : '11',
+            name: 'Anurag Roy',
+            position: 'Games and Sports Secretary',
+            imgurl: 'https://res.cloudinary.com/dei7rxeug/image/upload/v1710017062/minare/idmg45954qzm15giqs5h.jpg'
+        },
+
+        
+        
     ]
 
     return(
