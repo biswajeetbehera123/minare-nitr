25f29841b1019c9c322e8d3d0a3fd41269dcd534
Animesh239 <animeshbarik239@gmail.com>
2024-03-11 17:16:09 +0530

    auth final 2

--- src/components/Login.js
+++ src/components/Login.js
@@ -11,11 +11,33 @@ const Login = () => {
 
   const { user } = useContext(AuthContext)
  
-  const handleLoginWithGoogle = async () => {
+  // const handleLoginWithGoogle = async () => {
     const provider = new GoogleAuthProvider()
-    const res = await signInWithPopup(auth, provider)
-    console.log({res}, ['from Login.js']);
-  }
+  //   const res = await signInWithPopup(auth, provider)
+  //   console.log({res}, ['from Login.js']);
+  // }
+
+  const handleLoginWithGoogle = getAuth();
+  signInWithPopup(auth, provider)
+    .then((result) => {
+      // This gives you a Google Access Token. You can use it to access the Google API.
+      // const credential = GoogleAuthProvider.credentialFromResult(result);
+      // const token = credential.accessToken;
+      // // The signed-in user info.
+      // const user = result.user;
+      // // IdP data available using getAdditionalUserInfo(result)
+      // // ...
+      console.log(result.user)
+    }).catch((error) => {
+      // Handle Errors here.
+      // const errorCode = error.code;
+      // const errorMessage = error.message;
+      // // The email of the user's account used.
+      // const email = error.customData.email;
+      // // The AuthCredential type that was used.
+      // const credential = GoogleAuthProvider.credentialFromError(error);
+      // ...
+    });
 
   // useEffect(() => {
   //   if (user?.uid) {
