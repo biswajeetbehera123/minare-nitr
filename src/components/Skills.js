93e001d928e4a3b6f78a760a4dbe8adb495e16c2
Nidhi0604 <np060402@gmail.com>
2024-03-11 18:20:25 +0530

    FINAL PUSH ROUND 1 HOPEFULLY

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -1,18 +1,7 @@
-import meter1 from "../assets/sponsors/1.jpg";
-import meter2 from "../assets/sponsors/2.jpg";
-import meter3 from "../assets/sponsors/3.png";
-import meter4 from "../assets/sponsors/4.png";
-import meter5 from "../assets/sponsors/5.png";
-import meter6 from "../assets/sponsors/6.png";
-import meter7 from "../assets/sponsors/7.png";
-import meter8 from "../assets/sponsors/8.png";
-import meter9 from "../assets/sponsors/9.png";
-import meter10 from "../assets/sponsors/10.png";
 
 import Carousel from 'react-multi-carousel';
 import 'react-multi-carousel/lib/styles.css';
-import arrow1 from "../assets/img/arrow1.svg";
-import arrow2 from "../assets/img/arrow2.svg";
+
 import colorSharp from "../assets/img/color-sharp.png"
 
 export const Skills = () => {
@@ -45,47 +34,44 @@ export const Skills = () => {
                         <h2>Sponsors</h2>
                         <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.<br></br> Lorem Ipsum has been the industry's standard dummy text.</p>
                         <Carousel responsive={responsive} infinite={true} className="owl-carousel owl-theme skill-slider">
-                            <div className="item">
-                                <img src={meter1} alt="Image" />
+                        <div className="item">
+                                <img src={"https://res.cloudinary.com/daho6qhd0/image/upload/v1710151138/minare/im1_yjh1pv.png"} alt="Image" />
                                 <h5>OMC</h5>
                             </div>
                             <div className="item">
-                                <img src={meter2} alt="Image" />
-                                <h5>SmartChem Technologies Ltd</h5>
-                            </div>
-                            <div className="item">
-                                <img src={meter3} alt="Image" />
-                                <h5>OCPL</h5>
-                            </div>
-                            <div className="item">
-                                <img src={meter4} alt="Image" />
-                                <h5>IMFA</h5>
-                            </div>
-                            <div className="item">
-                                <img src={meter5} alt="Image" />
+                                <img src={"https://res.cloudinary.com/daho6qhd0/image/upload/v1710151428/minare/image_5_yzikr4.png"} alt="Image" />
                                 <h5>MCL</h5>
                             </div>
                             <div className="item">
-                                <img src={meter6} alt="Image" />
-                                <h5>RC Rungta Group</h5>
+                                <img src={"https://res.cloudinary.com/daho6qhd0/image/upload/v1710151255/minare/R_zsvmzk.png"} alt="Image" />
+                                <h5>RC RUNGTA GROUP</h5>
                             </div>
                             <div className="item">
-                                <img src={meter7} alt="Image" />
-                                <h5>SBI</h5>
-                            </div>
-                            <div className="item">
-                                <img src={meter8} alt="Image" />
+                                <img src={"https://res.cloudinary.com/daho6qhd0/image/upload/v1710151240/minare/JSW_uavz85.png"} alt="Image" />
                                 <h5>JSW</h5>
                             </div>
                             <div className="item">
-                                <img src={meter9} alt="Image" />
-                                <h5>MOIL Limited</h5>
+                                <img src={"https://res.cloudinary.com/daho6qhd0/image/upload/v1710151213/minare/image_3_ivc8wj.png"} alt="Image" />
+                                <h5>OCPL</h5>
+                                </div>
+                            
+                            <div className="item">
+                            <img src={"https://res.cloudinary.com/daho6qhd0/image/upload/v1710151151/minare/im2_zwpdx6.png"} alt="Image" />
+                                <h5>SMARTCHEM</h5>
                             </div>
                             <div className="item">
-                                <img src={meter10} alt="Image" />
-                                <h5>Adani Group</h5>
+                            <img src={"https://res.cloudinary.com/daho6qhd0/image/upload/v1710151378/minare/image_4_qhx2kn.png"} alt="Image" />
+                                <h5>IMFA</h5>
                             </div>
-                        </Carousel>
+                            <div className="item">
+                            <img src={"https://res.cloudinary.com/daho6qhd0/image/upload/v1710151194/minare/im3_kokoeu.jpg"} alt="Image" />
+                                <h5>moil</h5>
+                            </div>
+                            <div className="item">
+                            <img src={"https://res.cloudinary.com/daho6qhd0/image/upload/v1710151564/minare/ucil_gifzvz.jpg"} alt="Image" />
+                                <h5>ucil</h5>
+                            </div>
+                                </Carousel>
                     </div>
                 </div>
             </div>
